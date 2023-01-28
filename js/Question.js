0aa50479dc09a50cbaf7d0da3074112d8e50a4fd
messithegoat6 <115425305+messithegoat6@users.noreply.github.com>
2023-01-28 18:32:23 +0000

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -28,7 +28,7 @@ class Question {
     // this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     // this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     //this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    // question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+     question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
 
     this.question.position(150, 80);
     this.option1.html("1: Everyone " );
@@ -46,8 +46,8 @@ class Question {
 
     this.button.mousePressed(()=>{
       
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
-      // this.message.position(350, 350);
+       this.message.html("Thank You, Your Answer Has Been Submitted");
+       this.message.position(350, 350);
 
       // this.message.html("Thank You, Your Answer Has Been Submitted");
       // this.message.position();
